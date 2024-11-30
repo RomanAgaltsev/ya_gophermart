b69a60b4f79bd08300fc52cf46b9f330cf42c615
Roman Agaltsev <roman-agalcev@yandex.ru>
2024-11-30 15:29:08 +0300

    feat(logger): add slog+zap logger initialization

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/RomanAgaltsev/ya_gophermart
 
 go 1.22.7
+
+require (
+	go.uber.org/zap v1.27.0
+	go.uber.org/zap/exp v0.3.0
+)
+
+require go.uber.org/multierr v1.10.0 // indirect
---
+++ internal/logger/logger.go
@@ -0,0 +1,44 @@
+package logger
+
+import (
+	"log/slog"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/exp/zapslog"
+	"go.uber.org/zap/zapcore"
+)
+
+// Initialize initializes slog+zap logger.
+func Initialize() error {
+	encoderConfig := zapcore.EncoderConfig{
+		TimeKey:        "timestamp",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		MessageKey:     "message",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.CapitalLevelEncoder,
+		EncodeTime:     zapcore.ISO8601TimeEncoder,
+		EncodeDuration: zapcore.StringDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+	}
+	config := zap.Config{
+		Level:            zap.NewAtomicLevelAt(zap.InfoLevel),
+		Development:      true,
+		Encoding:         "console",
+		EncoderConfig:    encoderConfig,
+		OutputPaths:      []string{"stdout"},
+		ErrorOutputPaths: []string{"stderr"},
+	}
+
+	logger, err := config.Build()
+	if err != nil {
+		return err
+	}
+	defer func() { _ = logger.Sync() }()
+
+	slog.SetDefault(slog.New(zapslog.NewHandler(logger.Core(), nil)))
+
+	return nil
+}
