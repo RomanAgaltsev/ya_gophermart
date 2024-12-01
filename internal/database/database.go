fa0c8f6b9a40ba61d9025f11993d6ba8d058dc83
Roman Agaltsev <roman-agalcev@yandex.ru>
2024-12-01 16:34:39 +0300

    feat(database): add connection pool and migrations
    - create connection pool
    - do migrations

--- internal/database/database.go
+++ internal/database/database.go
@@ -37,7 +37,7 @@ func Migrate(ctx context.Context, dbpool *pgxpool.Pool, databaseURI string) {
 
 	// Set dialect
 	if err := goose.SetDialect("postgres"); err != nil {
-		slog.Error("goose: set dialect:", slog.String("error", err.Error()))
+		slog.Error("goose: set dialect", slog.String("error", err.Error()))
 	}
 
 	// Open connection from db pool
@@ -45,11 +45,11 @@ func Migrate(ctx context.Context, dbpool *pgxpool.Pool, databaseURI string) {
 
 	// Up migrations
 	if err := goose.UpContext(ctx, db, "."); err != nil {
-		slog.Error("goose: run migrations:", slog.String("error", err.Error()))
+		slog.Error("goose: run migrations", slog.String("error", err.Error()))
 	}
 
 	// Close connection
 	if err := db.Close(); err != nil {
-		slog.Error("goose: close connection:", slog.String("error", err.Error()))
+		slog.Error("goose: close connection", slog.String("error", err.Error()))
 	}
 }
