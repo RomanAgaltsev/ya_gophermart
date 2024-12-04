5e68b5505cf9d78623145149a427102aa33664fc
Roman Agaltsev <roman-agalcev@yandex.ru>
2024-12-04 06:12:54 +0300

    feat(auth): add JWTAuth constructor function

--- internal/pkg/auth/auth.go
+++ internal/pkg/auth/auth.go
@@ -10,25 +10,32 @@ import (
 type UserLogin string
 
 const (
-	// DefaultCookieName содержит имя куки по умолчанию.
+	// JWTSignAlgorithm contains JWT signing algorithm
+	JWTSignAlgorithm = "HS256"
+
+	// DefaultCookieName contains default cookie name.
 	DefaultCookieName = "jwt"
 
-	// DefaultCookiePath содержит путь куки по умолчанию.
+	// DefaultCookiePath contains default cookie path.
 	DefaultCookiePath = "/"
 
-	// DefaultCookieMaxAge содержит возраст куки по умолчанию.
+	// DefaultCookieMaxAge contains default cookie max age.
 	DefaultCookieMaxAge = 3600
 
-	// UserLoginClaimName содержит имя ключа логина пользователя в контексте.
+	// UserLoginClaimName contains key name of user login in a context.
 	UserLoginClaimName UserLogin = "login"
 )
 
-// NewJWTToken создает новый JWT токен.
+func NewAuth(secretKey string) *jwtauth.JWTAuth {
+	return jwtauth.New(JWTSignAlgorithm, []byte(secretKey), nil)
+}
+
+// NewJWTToken creates new JWT token.
 func NewJWTToken(ja *jwtauth.JWTAuth, login string) (token jwt.Token, tokenString string, err error) {
 	return ja.Encode(map[string]interface{}{string(UserLoginClaimName): login})
 }
 
-// NewCookieWithDefaults создает новую куку со значениями по умолчанию и переданным в параметре значением.
+// NewCookieWithDefaults creates new cookie with defaults and parameter value.
 func NewCookieWithDefaults(value string) *http.Cookie {
 	return &http.Cookie{
 		Name:     DefaultCookieName,
