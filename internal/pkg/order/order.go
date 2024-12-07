67f802b785c999df44adee6aa2f39ec3ebd01747
Roman Agaltsev <roman-agalcev@yandex.ru>
2024-12-07 12:51:27 +0300

    feat(order): check order number validness with Luhn algorithm

--- internal/pkg/order/order.go
+++ internal/pkg/order/order.go
@@ -1,12 +1,21 @@
 package order
 
-const (
-	StatusNew        = "NEW"
-	StatusProcessing = "PROCESSING"
-	StatusInvalid    = "INVALID"
-	StatusProcessed  = "PROCESSED"
-)
+import "strconv"
 
 func IsNumberValid(orderNumber string) bool {
-	return true
+	var sum int
+
+	parity := len(orderNumber) % 2
+
+	for i := 0; i < len(orderNumber); i++ {
+		digit, _ := strconv.Atoi(string(orderNumber[i]))
+		if i%2 == parity {
+			digit *= 2
+			if digit > 9 {
+				digit -= 9
+			}
+		}
+		sum += digit
+	}
+	return sum%10 == 0
 }
