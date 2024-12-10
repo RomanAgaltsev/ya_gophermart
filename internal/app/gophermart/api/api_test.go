9c3fb32076c8505acef66a91d9acd8a23ec93f19
Roman Agaltsev <roman-agalcev@yandex.ru>
2024-12-10 13:02:20 +0300

    test(handler): temporary comment

--- internal/app/gophermart/api/api_test.go
+++ internal/app/gophermart/api/api_test.go
@@ -5,8 +5,8 @@ import (
 	"encoding/json"
 	"net/http"
 
-	"github.com/RomanAgaltsev/ya_gophermart/internal/app/gophermart/api"
-	"github.com/RomanAgaltsev/ya_gophermart/internal/config"
+	//"github.com/RomanAgaltsev/ya_gophermart/internal/app/gophermart/api"
+	//"github.com/RomanAgaltsev/ya_gophermart/internal/config"
 	"github.com/RomanAgaltsev/ya_gophermart/internal/model"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -18,8 +18,8 @@ var _ = Describe("Handler", func() {
 	var (
 		server *ghttp.Server
 
-		cfg, _  = config.Get()
-		handler = api.NewHandler(cfg)
+		//	cfg, _  = config.Get()
+		//handler = api.NewHandler(cfg)
 
 		testUser = model.User{
 			Login:    "user",
@@ -37,9 +37,9 @@ var _ = Describe("Handler", func() {
 	})
 
 	Context("When POST request is sent to the /api/user/register path", func() {
-		BeforeEach(func() {
-			server.AppendHandlers(handler.UserRegistrion)
-		})
+		//		BeforeEach(func() {
+		//			server.AppendHandlers(handler.UserRegistrion)
+		//		})
 
 		It("Returns status OK (200) and a cookie", func() {
 			resp, err := http.Post(server.URL()+"/api/user/register", "application/json", bytes.NewReader(userBytes))
