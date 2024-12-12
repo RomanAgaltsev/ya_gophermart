3a4515a11627d26429c8cf4c905ec5bf420409f5
Roman Agaltsev <roman-agalcev@yandex.ru>
2024-12-12 15:36:37 +0300

    test(config): add config tests

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -10,26 +10,20 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-/*
-struct{
-	envName string
+type testCase struct {
+	envNam string
 	envVal string
-	flgName string
+	flgNam string
 	flgVal string
-	default string
+	defVal string
 }
-*/
 
-var _ = Describe("Config", Ordered, func() {
+var _ = Describe("Config", func() {
 	var cfg *config.Config
 	var err error
 
 	defaultArgs := os.Args
 	defaultCommandLine := flag.CommandLine
-
-	BeforeAll(func() {
-
-	})
 
 	AfterEach(func() {
 		os.Clearenv()
@@ -37,17 +31,19 @@ var _ = Describe("Config", Ordered, func() {
 		flag.CommandLine = defaultCommandLine
 	})
 
+	// Run address
+	ra := &testCase{
+		envNam: "RUN_ADDRESS",
+		envVal: "localhost:8081",
+		flgNam: "-a",
+		flgVal: "localhost:8091",
+		defVal: "localhost:8080",
+	}
+
 	DescribeTable("Run address",
 		func(envName, envVal, flgName, flgVal, def, expected string) {
-			if envName != "" {
-				t := GinkgoT()
-				t.Setenv(envName, envVal)
-			}
-
-			if flgName != "" {
-				flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
-				os.Args = append([]string{"cmd"}, flgName, flgVal)
-			}
+			setEnv(envName, envVal)
+			setFlag(flgName, flgVal)
 
 			cfg, err = config.Get()
 
@@ -56,16 +52,121 @@ var _ = Describe("Config", Ordered, func() {
 		},
 
 		EntryDescription("When env %s=%s, flag %s=%s and default=%s"),
-		Entry(nil, "RUN_ADDRESS", "localhost:8081", "-a", "localhost:8091", "localhost:8080", "localhost:8081"),
-		Entry(nil, "RUN_ADDRESS", "localhost:8081", "-a", "", "localhost:8080", "localhost:8081"),
-		Entry(nil, "RUN_ADDRESS", "localhost:8081", "", "", "localhost:8080", "localhost:8081"),
-		Entry(nil, "RUN_ADDRESS", "", "-a", "localhost:8091", "localhost:8080", "localhost:8091"),
-		Entry(nil, "", "", "-a", "localhost:8091", "localhost:8080", "localhost:8091"),
-		Entry(nil, "RUN_ADDRESS", "", "-a", "", "localhost:8080", ""),
-		Entry(nil, "", "", "-a", "", "localhost:8080", ""),
-		Entry(nil, "RUN_ADDRESS", "", "-a", "", "localhost:8080", ""),
-		Entry(nil, "RUN_ADDRESS", "", "", "", "localhost:8080", "localhost:8080"),
-		Entry(nil, "", "", "", "", "localhost:8080", "localhost:8080"),
-		Entry(nil, "", "", "", "", "localhost:8080", "localhost:8080"),
+		Entry(nil, ra.envNam, ra.envVal, ra.flgNam, ra.flgVal, ra.defVal, ra.envVal),
+		Entry(nil, ra.envNam, ra.envVal, ra.flgNam, "", ra.defVal, ra.envVal),
+		Entry(nil, ra.envNam, ra.envVal, "", "", ra.defVal, ra.envVal),
+		Entry(nil, ra.envNam, "", ra.flgNam, ra.flgVal, ra.defVal, ra.flgVal),
+		Entry(nil, "", "", ra.flgNam, ra.flgVal, ra.defVal, ra.flgVal),
+		Entry(nil, ra.envNam, "", ra.flgNam, "", ra.defVal, ""),
+		Entry(nil, "", "", ra.flgNam, "", ra.defVal, ""),
+		Entry(nil, ra.envNam, "", ra.flgNam, "", ra.defVal, ""),
+		Entry(nil, ra.envNam, "", "", "", ra.defVal, ra.defVal),
+		Entry(nil, "", "", "", "", ra.defVal, ra.defVal),
+		Entry(nil, "", "", "", "", ra.defVal, ra.defVal),
+	)
+
+	// Database URI
+	du := &testCase{
+		envNam: "DATABASE_URI",
+		envVal: "postgres://postgres:12345@localhost:5432/gophermart?sslmode=disable",
+		flgNam: "-d",
+		flgVal: "postgres://postgres:12346@localhost:5433/gophermart?sslmode=disable",
+		defVal: "",
+	}
+
+	DescribeTable("Database URI",
+		func(envName, envVal, flgName, flgVal, def, expected string) {
+			setEnv(envName, envVal)
+			setFlag(flgName, flgVal)
+
+			cfg, err = config.Get()
+
+			Expect(err).Should(BeNil())
+			Expect(cfg.DatabaseURI).To(Equal(expected))
+		},
+
+		EntryDescription("When env %s=%s, flag %s=%s and default=%s"),
+		Entry(nil, du.envNam, du.envVal, du.flgNam, du.flgVal, du.defVal, du.envVal),
+		Entry(nil, du.envNam, du.envVal, du.flgNam, "", du.defVal, du.envVal),
+		Entry(nil, du.envNam, du.envVal, "", "", du.defVal, du.envVal),
+		Entry(nil, du.envNam, "", du.flgNam, du.flgVal, du.defVal, du.flgVal),
+		Entry(nil, "", "", du.flgNam, du.flgVal, du.defVal, du.flgVal),
+		Entry(nil, du.envNam, "", du.flgNam, "", du.defVal, ""),
+		Entry(nil, "", "", du.flgNam, "", du.defVal, ""),
+		Entry(nil, du.envNam, "", du.flgNam, "", du.defVal, ""),
+		Entry(nil, du.envNam, "", "", "", du.defVal, du.defVal),
+		Entry(nil, "", "", "", "", du.defVal, du.defVal),
+		Entry(nil, "", "", "", "", du.defVal, du.defVal),
+	)
+
+	// Accrual system address
+	asa := &testCase{
+		envNam: "ACCRUAL_SYSTEM_ADDRESS",
+		envVal: "localhost:9081",
+		flgNam: "-r",
+		flgVal: "localhost:9091",
+		defVal: "",
+	}
+
+	DescribeTable("Accrual system address",
+		func(envName, envVal, flgName, flgVal, def, expected string) {
+			setEnv(envName, envVal)
+			setFlag(flgName, flgVal)
+
+			cfg, err = config.Get()
+
+			Expect(err).Should(BeNil())
+			Expect(cfg.AccrualSystemAddress).To(Equal(expected))
+		},
+
+		EntryDescription("When env %s=%s, flag %s=%s and default=%s"),
+		Entry(nil, asa.envNam, asa.envVal, asa.flgNam, asa.flgVal, asa.defVal, asa.envVal),
+		Entry(nil, asa.envNam, asa.envVal, asa.flgNam, "", asa.defVal, asa.envVal),
+		Entry(nil, asa.envNam, asa.envVal, "", "", asa.defVal, asa.envVal),
+		Entry(nil, asa.envNam, "", asa.flgNam, asa.flgVal, asa.defVal, asa.flgVal),
+		Entry(nil, "", "", asa.flgNam, asa.flgVal, asa.defVal, asa.flgVal),
+		Entry(nil, asa.envNam, "", asa.flgNam, "", asa.defVal, ""),
+		Entry(nil, "", "", asa.flgNam, "", asa.defVal, ""),
+		Entry(nil, asa.envNam, "", asa.flgNam, "", asa.defVal, ""),
+		Entry(nil, asa.envNam, "", "", "", asa.defVal, asa.defVal),
+		Entry(nil, "", "", "", "", asa.defVal, asa.defVal),
+		Entry(nil, "", "", "", "", asa.defVal, asa.defVal),
+	)
+
+	// Secret key
+	sk := &testCase{
+		envNam: "SECRET_KEY",
+		envVal: "very secret key",
+		defVal: "secret",
+	}
+
+	DescribeTable("Secret key",
+		func(envName, envVal, def, expected string) {
+			setEnv(envName, envVal)
+
+			cfg, err = config.Get()
+
+			Expect(err).Should(BeNil())
+			Expect(cfg.SecretKey).To(Equal(expected))
+		},
+
+		EntryDescription("When env %s=%s and default=%s"),
+		Entry(nil, sk.envNam, sk.envVal, sk.defVal, sk.envVal),
+		Entry(nil, sk.envNam, "", sk.defVal, sk.defVal),
+		Entry(nil, "", "", sk.defVal, sk.defVal),
 	)
 })
+
+func setEnv(name, value string) {
+	if name != "" {
+		t := GinkgoT()
+		t.Setenv(name, value)
+	}
+}
+
+func setFlag(name, value string) {
+	if name != "" {
+		flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+		os.Args = append([]string{"cmd"}, name, value)
+	}
+}
